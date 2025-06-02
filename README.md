6cd44f5dc21fe21d5601f7c479e4979050bb69a0
pavel <pavelpichugin28@gmail.com>
2025-06-02 16:50:33 +0200

    final touches

---
+++ README.md
@@ -0,0 +1,11 @@
+Задачник для ТЗ FastAPI (backend) и Vue 3 + Vite (frontend) + pgsql (для теста sqlite)
+
+git clone https://github.com/Pavelpich/python-junior-tracker-solution-Pavel-Pichugin.git
+cd tack-app
+
+docker-compose up --build
+
+после запуска frontend: http://localhost
+backend: http://localhost/api
+
+Node.js 18+ и Python 3.10+
--- task-app/backend/app/main.py
+++ task-app/backend/app/main.py
@@ -1,9 +1,16 @@
-# backend/app/main.py
 from fastapi import FastAPI
-from app.api.routes import router as tasks_router
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.database import engine, Base
 
 app = FastAPI(title="Task API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost", "http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # автоматическое создание таблиц при запуске
@@ -11,6 +18,8 @@ app = FastAPI(title="Task API")
 def create_tables() -> None:
     Base.metadata.create_all(bind=engine)
 
+
+from app.api.routes import router as tasks_router
 
 app.include_router(
     tasks_router,
